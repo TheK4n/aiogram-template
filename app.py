ea7524f25d3a1c9bed27c22e78ac70c757692e6e
Pendosv <djvlad967891@gmail.com>
2021-09-11 22:06:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,6 @@ async def on_shutdown(dispatcher):
 
     dispatcher.stop_polling()
 
-    # закрывает бд
-    db.close()
-
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
